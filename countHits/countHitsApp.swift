401c38aaa7588d4e1bb5a5a35dcea2d04b549006
Yuki Yoshida <onedyuki520@gmail.com>
2023-07-09 23:12:15 +0900

    Initial Commit

---
+++ countHits/countHitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  countHitsApp.swift
+//  countHits
+//
+//  Created by 吉田侑己 on 2023/07/09.
+//
+
+import SwiftUI
+
+@main
+struct countHitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
